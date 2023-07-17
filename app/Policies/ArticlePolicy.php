da637f67a692beb257ffd0d0ad596e0467a43a64
Samy Reda <s.reda@intrazero.net>
2023-07-17 11:41:41 +0300

    article policy modified

--- app/Policies/ArticlePolicy.php
+++ app/Policies/ArticlePolicy.php
@@ -22,6 +22,14 @@ class ArticlePolicy
     }
 
     /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
      * Determine whether the user can update the model.
      */
     public function update(User $user, Article $article): bool
